163e8465b9ebc7d6b051e0b4fe71609612c38d2f
Martin Michalski <martinm6@dmserv21.cs.illinois.edu>
2022-02-21 14:04:18 -0600

    Updated the run_emb_part_tax bash script to be able to better interface from an overall runnning script

--- c/run_emb_part_tax.sh
+++ c/run_emb_part_tax.sh
@@ -1,11 +1,28 @@
-# dataset directory
-dataset=../dblp
+# process command line arguments (support for flags)
+while getopts d:t:f: flag
+do
+    case "${flag}" in
+        d) dataset=../${OPTARG};;
+        t) threads=${OPTARG};;
+        f) topic_file=${OPTARG};; 
+    esac
+done
+
+# set defaults if arguments are not passed
+dataset="${dataset:-../dblp}"
+threads="${threads:-20}"
+topic_file="${topic_file:-field}"
+
+# print settable parameters
+echo "Launching Partial Concept Learning"
+echo "    Dataset:           ${dataset}"
+echo "    Threads:           ${threads}"
+echo "    Topic File Suffix: ${topic_file}"
 
 # text file name; one document per line
 text_file=phrase_text.txt
 
-topic_file=field
-
+# topic file suffix
 output_file=${topic_file}
 
 emb_file=emb_part_${output_file}
@@ -13,11 +30,10 @@ emb_file=emb_part_${output_file}
 make train_emb_part_tax
 
 # default:
-# reg_lambda = 10
-# global_lambda = 1.5
-# window = 5
-# pretrain = 5
-
-./train_emb_part_tax -train ./${dataset}/${text_file} -output ./${dataset}/${emb_file}_w.txt -kappa ./${dataset}/${emb_file}_cap.txt -topic ./${dataset}/topics_${topic_file}.txt -topic_output ./${dataset}/${emb_file}_t.txt -doc_output ./${dataset}/${emb_file}_s.txt -reg_lambda 1 -cbow 0 -size 100 -global_lambda 1.5 -window 5 -negative 5 -sample 1e-3 -min-count 50 -threads 20 -binary 0 -iter 10 -pretrain 5 -rank_product 0 -gen_vocab 0 -load_emb 0
+#     reg_lambda = 10
+#     global_lambda = 1.5
+#     window = 5
+#     pretrain = 5
+./train_emb_part_tax -train ./${dataset}/${text_file} -output ./${dataset}/${emb_file}_w.txt -kappa ./${dataset}/${emb_file}_cap.txt -topic ./${dataset}/topics_${topic_file}.txt -topic_output ./${dataset}/${emb_file}_t.txt -doc_output ./${dataset}/${emb_file}_s.txt -reg_lambda 1 -cbow 0 -size 100 -global_lambda 1.5 -window 5 -negative 5 -sample 1e-3 -min-count 50 -threads ${threads} -binary 0 -iter 10 -pretrain 5 -rank_product 0 -gen_vocab 0 -load_emb 0
 
 		
