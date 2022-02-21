00637724f9fe8b80b1149090437ebd7c9723f24e
Martin Michalski <martinm6@dmserv21.cs.illinois.edu>
2022-02-21 15:09:10 -0600

    Template for the overall CoRel run script created

---
+++ run.sh
@@ -0,0 +1,17 @@
+# Arguments
+#   -t/--threads:   how many cpu threads may be launched
+#   -g/--gpu:       which gpu is free for use
+#   -s/--seed_file: which seed file to use, filename must match topics_[a-zA-Z0-9]*.txt regex
+#   -d/--dataset:   which dataset to use
+
+# Parsing of input arguments
+
+# Concept learning on the passed seed
+
+# Relation transferring
+
+# Concept learning on the output of the relation transferring step
+
+# Post-processing of the final results into a single taxonomy description file
+
+# Generation of a visualization of the output taxonomy
