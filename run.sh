ddd39ea4a7488a61710bd7566e947bc2794b7a3b
Martin Michalski <martinm6@dmserv21.cs.illinois.edu>
2022-02-24 02:26:08 -0600

    Run.sh template

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@
 #   -s/--seed_file: which seed file to use, filename must match topics_[a-zA-Z0-9]*.txt regex
 #   -d/--dataset:   which dataset to use
 
-# Parsing of input arguments
+# -- Parsing of input arguments -- 
 
-# Concept learning on the passed seed
+# -- Concept learning on the passed seed --
 
-# Relation transferring
+# -- Relation transferring -- 
 
-# Concept learning on the output of the relation transferring step
+# -- Concept learning on the output of the relation transferring step -- 
 
-# Post-processing of the final results into a single taxonomy description file
+# -- Post-processing of the final results into a single taxonomy description file -- 
 
-# Generation of a visualization of the output taxonomy
+# --  Generation of a visualization of the output taxonomy --
